704e2624521bed078f05cc67ca2db54e94b6637a
katerynukr <kateryna_neholiuk@ukr.net>
2021-04-06 19:59:19 +0300

    first commit

---
+++ LibraryApplication/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApplication.Models
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string[] Author { get; set; }
+        public string[] Category { get; set; }
+        public string Language { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string ISBN { get; set; }
+        public Customer Customer { get; set; }
+        public bool IsTaken = false;
+
+    }
+}
